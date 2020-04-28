2fdde408fe8684212abcb1731eadd47cebe2316c
himself65 <himself65@outlook.com>
2020-04-28 22:17:26 +0800

    web: update AppBar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Container, CssBaseline, AppBar, Toolbar, Typography } from '@material-ui/core'
 
 function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6'>
+            COVID-19
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth='xs'>
+        <CssBaseline/>
+      </Container>
     </div>
   )
 }
